56c328ae6d18b82a0c8dbdf99711b8f1016bf3bf
ranbudayo <45906970+ranbudayo@users.noreply.github.com>
2025-01-29 19:29:12 +0900

    [update] スタイルの変更機能　仮実装

---
+++ styles/styles.go
@@ -0,0 +1,10 @@
+package styles
+
+var ThemeList = []string{
+	"\ntheme: default\nclass: lead\n",
+	"\ntheme: default\nclass: lead invert\n",
+	"\ntheme: gaia\nclass: lead\n",
+	"\ntheme: gaia\nclass: lead invert\n",
+	"\ntheme: uncover\nclass: lead\n",
+	"\ntheme: uncover\nclass: lead invert\n",
+}
